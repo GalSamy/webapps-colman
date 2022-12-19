a7969cdb94ebc3c7b17370dc6566b9e9538d8217
galsamy <galsamytl@gmail.com>
2022-12-19 21:35:50 +0200

    finsihed

--- MemoryProject/scripts/index.js
+++ MemoryProject/scripts/index.js
@@ -6,6 +6,7 @@ var cardContainer = document.getElementsByClassName("card-container")
 let clickedPair=-1
 let lastCard
 let amountCardsClicked=0
+let totalTimeSeconds;
 $(document).ready(() =>{
     console.log("document ready")
 })
@@ -16,10 +17,20 @@ function change(){
     $("#amount").val(cardAmount)
 }
 function gameInit(){
+    let name = $(`#name`).val()
+    console.log(name)
+
+    if (cardAmount == null || name === ""){
+        alert("please insert name and number of cards")
+        return;
+    }
     amountCardsClicked = 3
     
     $("#main-form").css("cssText", "display : none !important")
-    $("#Title").append(`You Have ${(cardAmount / 2)} Seconds To Look At The Cards!`)
+    let p = document.createElement("h1")
+    p.textContent = `You Have ${(cardAmount / 2)} Seconds To Look At The Cards!`
+    p.id = "pTitle"
+    $("#Title").append(p)
 
 
     for(let i=0.5;i<=cardAmount/2;i+=0.5)
@@ -35,27 +46,49 @@ function gameInit(){
     cardContainer[0].appendChild(row)
     createCards()
     let i = 0;
+    const interval = setInterval(function (){
+
+        $("#pTitle").text(`You Have ${(cards.length / 2) - i} Seconds To Look At The Cards!`)
+        i++;
+    }, 1000)
 
     const timeout = setTimeout(() =>{
+
         clearInterval(interval)
         for (const card in cardsIds){
             flipCard(card)
         }
-        $("#Title").text("Time : 0")
+        console.log("pTitle : " + $("#pTitle").toString())
+
+        $("#pTitle").text("Time : 0")
         let time = 0;
-        const stoper = setInterval(() =>{
-            $("#Title").text(`Time : ${time}`)
+        const stopper = setInterval(() =>{
+            $("#pTitle").text(`Time : ${time}`)
             time++
+
+        },1000)
+        const checker = setInterval(() =>{
+            if (cardAmount === 0) {
+                totalTimeSeconds = time;
+                $(`#cards-row`).remove()
+                $(`#pTitle`).text(`Good job, ${name}, You completed the game in ${totalTimeSeconds - 2} seconds`)
+                let btn = document.createElement("button")
+                let btnDiv = document.createElement("div")
+                btnDiv.className = "d-flex justify-content-center"
+                btn.className = "btn btn-success justify-content-center"
+                btn.innerText = "Play Again!"
+                btn.id = "endBtn"
+                btn.setAttribute("onclick","initAgain(this)")
+                document.body.appendChild(btnDiv)
+                btnDiv.appendChild(btn)
+                clearInterval(stopper)
+                console.log("Cleared stopper")
+                clearInterval(checker)
+            }
         },1000)
         amountCardsClicked = 0
     }, (cards.length / 2 + 1) * 1000)
 
-    const interval = setInterval(function (){
-
-
-            $("#Title").text(`You Have ${(cards.length / 2) - i} Seconds To Look At The Cards!`)
-            i++;
-        }, 1000)
 
 }
 function createCards(){
@@ -68,7 +101,8 @@ function createCards(){
     console.log(cards)
     let row = document.createElement('div')
     shuffle()
-    row.className = 'row'
+    row.className = 'row xl-5 l-3 m-2 justify-content-center'
+    row.id = "cards-row"
     cards.forEach(card => {
         row.appendChild(card)
     });
@@ -121,10 +155,9 @@ function clicked(card){
             setTimeout(()=>{
             //card.style.backgroundColor=lastCard.style.backgroundColor
             clickedPair = -1
-            $(`#${lastCard.id}`).css("cssText", "visibility: hidden !important")
-            $(`#${card.id}`).css("cssText", "visibility: hidden !important")
             amountCardsClicked=0
             },500)
+            cardAmount -= 2
         }
         else{
             console.log('case 4')
@@ -147,6 +180,7 @@ function randomColor()
 function createCard(pair,id){
 
     let card = document.createElement('div')
+    let imgPath= "./assets/"+pair+'.png'
     card.className =  'card col-2'
     card.setAttribute('id',id)
     card.setAttribute("onclick","clicked(this)")
@@ -154,6 +188,14 @@ function createCard(pair,id){
     card.style.margin="1rem"
     card.style.height="10rem"
     card.style.width="18rem"
+
+    let img = document.createElement('img')
+    img.src=imgPath
+    img.className="card-img-top m-2"
+    img.id=`card-img-${id}`
+    img.style.width='100%'
+    img.width = 200
+    img.height = 150
     
     let cardBody = document.createElement('div');
     cardBody.className = 'card-body';
@@ -161,11 +203,11 @@ function createCard(pair,id){
 
     let title = document.createElement('h5');
     title.innerText = pair;
-    title.className = 'card-title';
-    title.id = `cardTitle-${id}`
-    cardBody.appendChild(title)
-    card.appendChild(cardBody)
-
+   // title.className = 'card-title';
+   // title.id = `cardTitle-${id}`
+   // cardBody.appendChild(title)
+    card.appendChild(img)
+    //card.appendChild(cardBody)
     return card
     // let cardBody = document.createElement('div');
     // cardBody.className = 'card-body';
@@ -186,13 +228,19 @@ function createCard(pair,id){
 function flipCard(card) { // flipped is false by default
     console.log(card)
     if(flippedMap.get(card)){ // flipped is true => hidden
-        $(`#cardTitle-${card}`).css("cssText", "display : flex !important") 
-        $(`#cardBody-${card}`).css("cssText", "display : flex !important")
+        $(`#card-img-${card}`).css("cssText", "display : flex !important")
+      //  $(`#cardBody-${card}`).css("cssText", "display : flex !important")
         flippedMap.set(card, false);
     }else{ // hide
-        $(`#cardTitle-${card}`).css("cssText","display : none !important")
-        $(`#cardBody-${card}`).css("cssText","display : none !important")
+        $(`#card-img-${card}`).css("cssText","display : none !important")
+      //  $(`#cardBody-${card}`).css("cssText","display : none !important")
         flippedMap.set(card,true);
         console.log(`flipped ${card}`)
     }
+
+
 } //
+
+function initAgain(button){
+    location.reload()
+}
